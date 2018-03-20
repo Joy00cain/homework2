93481292ed13f366d4f84347870e546e41a888dd
joy00cain <zhao.2259@osu.edu>
2018-03-19 22:45:36 -0400

    Answer to question 6

--- code of homework2.R
+++ code of homework2.R
@@ -10,10 +10,27 @@ gun_deaths %>%
 
 # answer to question 3
 gun_deaths %>%
-     group_by(sex) %>%
-     summarise(avgage = mean(age, na.rm = TRUE))
+   group_by(sex) %>%
+   summarise(avgage = mean(age, na.rm = TRUE))
 # generate pic of this data
-ggplot(aes(x = sex))+
-            geom_bar( aes(y = avgage), stat = "identity")
+   ggplot(aes(x = sex))+
+   geom_bar( aes(y = avgage), stat = "identity")
 
 # answer to question 4
+   filter(gun_deaths, sex == "M", race == "White", year == 2012, education != "Less than HS", education != "NA")
+   
+# answer to question 5
+   gun_deaths %>%
+ #generate catogory variables for month
+     mutate(category = cut(month, breaks = c(0,3,6,9,12), labels = c("winter","spring","summer","fall"))) %>%
+     
+    ggplot(aes(x = category))+
+    geom_bar()+
+    labs( title = "Gun Death in Different Seasons",
+          x = "Season",
+          y = "Number of Gun Death")
+     
+     
+     
+   
+   
